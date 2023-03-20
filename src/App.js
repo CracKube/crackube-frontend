e480d891119daad1c3e7626c91cc95b97a866822
Hariharan K <harry@Hariharans-MacBook-Pro.local>
2023-03-20 17:09:27 +0530

    Committed

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Sidebar1 from './components/Sidebar/Sidebar1';
 import Sidebar2 from './components/Sidebar/Sidebar2';
 import AddButton from './components/AddButton/AddButton';
 import Wallet from './components/Wallet/Wallet';
+import BlogPart from './components/BlogPart/BlogPart';
 
 function App() {
   return (
     <div>
-      <Wallet />
-      <Sidebar1 />
+      {/* <Sidebar1 /> */}
       <MainBar />
-      <Sidebar2 />
+      {/* <Sidebar2 /> */}
       <AddButton />
     </div>
   );
