b9ac162d45151b6e2e1b55e78c996c24be68284d
har3x3 <har3x3@gmail.com>
2023-03-20 23:46:20 +0530

    Hello there

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 
-<<<<<<< HEAD
-import MainBar from './components/MainBar/MainBar';
-import Sidebar1 from './components/Sidebar/Sidebar1';
-import Sidebar2 from './components/Sidebar/Sidebar2';
-import AddButton from './components/AddButton/AddButton';
-import Wallet from './components/Wallet/Wallet';
-import BlogPart from './components/BlogPart/BlogPart';
-=======
 import {MainBar, AddButton, BlogPart} from './Component/Constants'
->>>>>>> main
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-      {/* <Sidebar1 /> */}
       <MainBar />
-      {/* <Sidebar2 /> */}
-=======
-      <MainBar />
->>>>>>> main
       <AddButton />
     </div>
   );
