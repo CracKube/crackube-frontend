a65f6dfa394bb87dfd4b8a5375822ef288e6eada
Geen-19 <angelinedias53@gmail.com>
2023-05-20 15:31:42 +0530

    Email-Settings Done

--- src/Component/Settings/Email.js
+++ src/Component/Settings/Email.js
@@ -45,6 +45,24 @@ function Email() {
    </div>
    
 </div>
+<div className='about-sep'>
+<div className='my-details'><p>Email</p></div>
+   <div className='email-prof'>
+       <div className='prof-cover'>
+       <p>Current Email Address</p>
+       <form action=""><input type="text" /></form>
+       <p>New Email Address</p>
+       <form action=""><input type="text" /></form>
+       <p>Confirm New Email Address</p>
+       <form action=""><input type="text" /></form>
+       </div>
+       <div className='email-desc'>
+           <p>For your security we will send a verification link to danielcooper@gmail.com. This update will not take place until <br /> you follow the instructions listed in that email. <br />
+If you do not have access to danielcooper@gmail.com, please contact support for assistance.</p>
+           </div>
+           <div className='update-btn'><button>Update</button></div>
+    </div>
+</div>
 
 </div>
 
