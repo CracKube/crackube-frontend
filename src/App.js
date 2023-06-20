82906e14cfbc7739bc3df954af93e59bc1e98ddf
Geen-19 <angelinedias53@gmail.com>
2023-06-20 10:26:04 +0530

    Settings

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import Signup from "./Component/Pages/Signup.jsx";
 import Home from "./Component/Pages/Home";
 import OTP  from "../src/Component/ForgotPassword/ForgotPassword.js"
 import Search from "../src/Component/SearchPage/SearchPage.jsx";
-
-
+import Settings from '../src/Component/Settings/SettingsMenu';
+import MyDetails from '../src/Component/Settings/MyDetails.js';
+import Security from '../src/Component/Settings/Security.js';
+import Email from '../src/Component/Settings/Email.js';
+import Password from '../src/Component/Settings/Password.js';
+import DeleteAccount from './Component/Settings/DeleteAccount';
 function App() {
   return (
     <>
@@ -22,7 +26,14 @@ function App() {
               <Route path="/home" element={<Home/>}></Route>
               <Route path = "/otp" element= {<OTP/>}> </Route>
               <Route path = "/search" element = {<Search/>} ></Route>
+              <Route path = "/settings" element = {<Settings/>}></Route>
+              <Route path = "/my-details" element = {<MyDetails/>}></Route>
+              <Route path = "/security" element = {<Security/>}></Route>
+              <Route path = "/email" element = {<Email/>}></Route>
+              <Route path = "/password" element = {<Password/>}></Route>
+              <Route path = "/delete-account" element = {<DeleteAccount/>}></Route>
             </Routes>
+            
         </Router>
         {/* <MainBar /> */}
       </div>
