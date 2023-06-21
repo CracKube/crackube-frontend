7e081b82ac3690e17568346b3d111889cdc48b8c
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-06-21 12:52:05 +0530

    Fix : + Button

--- src/Component/AddButton/AddButton.jsx
+++ src/Component/AddButton/AddButton.jsx
@@ -6,37 +6,38 @@ import { useState } from 'react';
 // add two numbers
 
 
-export default function AddButton(){
-    const [showPopup, setShowPopup] = useState(false);
-    const listItems = ['Item 1', 'Item 2', 'Item 3'];
+export default function AddButton() {
+  const [showPopup, setShowPopup] = useState(false);
+  const listItems = ['Item 1', 'Item 2', 'Item 3'];
+  const buttonHandler = ('click', () => {
     const button = document.querySelector(".addbutton");
-    const buttonHandler  = ('click', () => {
-        button.classList.add('rotate-animation');
-        setShowPopup(!showPopup);
-        setTimeout(() => {
-          button.classList.remove('rotate-animation');
-        }, 1000); // Remove the class after the animation duration (1 seconds)
-      });
-    return(
-       
-        <>
-            <button  className='addbutton'
-            onClick={ buttonHandler}>
-                <img src={addbutton} alt="" />
-            </button>
-            {showPopup && (
-            <div className="popup">
-            <ul>
-              <div>
-                {listItems.map((item, index) => (
+    if (!button) return;
+    button.classList.add('rotate-animation');
+    setShowPopup(!showPopup);
+    setTimeout(() => {
+      button.classList.remove('rotate-animation');
+    }, 1000); // Remove the class after the animation duration (1 seconds)
+  });
+  return (
+
+    <>
+      <button className='addbutton'
+        onClick={buttonHandler}>
+        <img src={addbutton} alt="" />
+      </button>
+      {showPopup && (
+        <div className="popup">
+          <ul>
+            <div>
+              {listItems.map((item, index) => (
                 <div className='icon-style' key={index}>{item}</div>
-                ))}
-              </div>
-            </ul>
+              ))}
             </div>
+          </ul>
+        </div>
       )}
-        </>
-    );
+    </>
+  );
 }
 // import React, { useState } from 'react';
 
