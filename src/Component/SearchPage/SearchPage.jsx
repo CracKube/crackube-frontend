e896c84bd35d3f16f934e27371c0c2f22b533a96
Anjali Kedia <24kedia.anjali@gmail.com>
2023-06-21 16:44:13 +0530

    Search-bar suggestion working

--- src/Component/SearchPage/SearchPage.jsx
+++ src/Component/SearchPage/SearchPage.jsx
@@ -1,33 +1,31 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Search from '../../Assets/search.svg'
 import { Link } from 'react-router-dom'
 import MenuBar from '../MenuBar'
 import { MainBar, TopNavBar } from '../Constants'
 import homeIcon from '../../Assets/homeIcon.svg'
 
-const SearchBar = ({setResults})=>{
+
+const SearchBar = ({handleChange})=>{
   const [input, setInput ] = useState("");
   
   
-    const fecthData = (value) => {
-      fetch("https://jsonplaceholder.typicode.com/users").then((response) => response.json()).then(json =>{
-        // console.log(json);
-        const results = json.filter((user) => {
-          return value && user && user.name && user.name.toLowerCase().includes(value)
-        });
-        setResults(results);
+    // const fecthData = (value) => {
+    //   fetch("https://crackube-backend-test.onrender.com/questions/get").then((response) => response.json()).then(json =>{
+    //     // console.log(json);
+    //     const results = json.filter((user) => {
+    //       return value && user && user.questionBody && user.questionBody.toLowerCase().includes(value)
+    //     });
+    //     setResults(results);
         
-      } )
-    }
-    const handleChange = (value) => {
-      setInput(value)
-      fecthData(value)
-    }
+    //   } )
+    // }
+
   return (
     <div className='Search'>
     <img src= {Search} alt="img" />    
     <input className='search-bar' 
-    type="text" value={input} onChange={(e) => handleChange(e.target.value)}
+    type="text" value={input} onChange={(e) => {setInput(e.target.value);handleChange(e.target.value)}}
     placeholder='Search for anything'/>
     <button className='search-button'>Search</button>
 
@@ -40,20 +38,80 @@ const SearchResultsList =({results}) => {
     <div className='results-list'>
       {
         results.map((result,id)=>{
-          return <div key={id}>{result.name}</div>
+          return <SearchResult result={result} key={id}/>
         })
       }
     </div>
   )
 }
 
+const SearchResult =({result})=> {
+  return (
+  <div className='search-result'>
+    
+    <img className='search-list-icon'src= {Search} alt="img" />
+    {result}</div>
+  )
+
+}
 
 
 
 function SearchPage () {
-  
-  const [results, setResults] = useState([]);
- 
+  const [input, setInput] = useState('')
+  const [results, setResults] = useState([])
+  const [blogs, setBlogs] = useState([])
+  const [questions, setQuestions] = useState([])
+  const [users, setUsers] = useState([])
+
+
+  const getData = async () => {
+    const response1 = await fetch('https://crackube-backend-test.onrender.com/blogs/getAll');
+    const data1 = await response1.json();
+    setBlogs(data1)
+
+    const response2 = await fetch('https://crackube-backend-test.onrender.com/questions/get');
+    const data2 = await response2.json();
+    setQuestions(data2)
+
+    const response3 = await fetch('https://crackube-backend-test.onrender.com/users/getUsers');
+    const data3 = await response3.json();
+    setUsers(data3)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const handleChange = (value) => {
+    setInput(value)
+    if (value == "") {
+      setResults([])
+      return
+    }
+
+    let newResult = []
+
+    blogs.forEach(blog => {
+      if (blog.blogTitle.toLowerCase().includes(value.toLowerCase())) {
+        newResult.push(blog.blogTitle)
+      }
+    })
+
+    questions.forEach(question => {
+      if (question.questionBody.toLowerCase().includes(value.toLowerCase())) {
+        newResult.push(question.questionBody)
+      }
+    })
+
+    // users.forEach(user => {
+    //   if (user.firstName.toLowerCase().includes(value.toLowerCase())) {
+    //     newResult.push(user.firstName)
+    //   }
+    // })
+
+    setResults(newResult)
+  }
   
   return (
 
@@ -78,7 +136,7 @@ function SearchPage () {
             
         </div>
        {/* SearchBar */}
-       <SearchBar setResults={setResults}/>
+       <SearchBar handleChange={handleChange}/>
        <SearchResultsList results={results}/>
         <div className='coverUp'>
         <div ><p>Discover</p></div>
