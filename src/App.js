6fdeb3c1c3785b10fc011275303343759c90db27
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-08-22 12:04:01 +0530

    OTP page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LandingPage from "./Component/LandingPage/LandingPage";
 import Login from "./Component/Pages/Login";
 import Signup from "./Component/Pages/Signup.jsx";
 import Home from "./Component/Pages/Home";
-import OTP from "../src/Component/ForgotPassword/ForgotPassword.js"
+import OTP from "../src/Component/OTP Page/OTP"
 import Search from "../src/Component/SearchPage/SearchPage.jsx";
 import Settings from '../src/Component/Settings/SettingsMenu';
 import MyDetails from '../src/Component/Settings/MyDetails.js';
@@ -15,7 +15,6 @@ import DeleteAccount from './Component/Settings/DeleteAccount';
 import UploadPage from './Component/UploadPage/UploadPage';
 import BlogPage from './Component/BlogPage/BlogPage.js';
 import AnsweringPage from './Component/AnsweringPage/AnsweringPage';
-import {useAuth0} from '@auth0/auth0-react';
 import { useState } from 'react';
 import { createContext } from 'react'
 import ProfilePage from './Component/profile_page/ProfilePage';
@@ -23,11 +22,11 @@ export const ThemeContext = createContext(null)
 
 function App() {
     const [theme, setTheme] = useState("light")
-    
+
     return (
-            <div className="body">
-                <AddButton />
-                <Router>
+        <div className="body">
+            <AddButton />
+            <Router>
                 <ThemeContext.Provider >
                     <Routes>
                         <Route path="/" element={<LandingPage />}></Route>
@@ -35,23 +34,23 @@ function App() {
                         <Route path="/login" element={<Login />}></Route>
                         <Route path="/home" element={<Home />}></Route>
                         <Route path="/otp" element={<OTP />}> </Route>
-                        <Route path="/search" element={<Search theme = {theme} setTheme = {setTheme}/>} ></Route>
-                        <Route path="/settings" element={<Settings  theme = {theme} setTheme = {setTheme}/>}></Route>
-                        <Route path="/my-details" element={<MyDetails theme = {theme} setTheme = {setTheme}/>}></Route>
-                        <Route path="/security" element={<Security theme = {theme} setTheme = {setTheme}/>}></Route>
-                        <Route path="/email" element={<Email theme = {theme} setTheme = {setTheme}/>}></Route>
-                        <Route path = "/profile" element = {<ProfilePage theme = {theme} setTheme = {setTheme}/>}></Route>
-                        <Route path="/password" element={<Password theme = {theme} setTheme = {setTheme}/>}></Route>
+                        <Route path="/search" element={<Search theme={theme} setTheme={setTheme} />} ></Route>
+                        <Route path="/settings" element={<Settings theme={theme} setTheme={setTheme} />}></Route>
+                        <Route path="/my-details" element={<MyDetails theme={theme} setTheme={setTheme} />}></Route>
+                        <Route path="/security" element={<Security theme={theme} setTheme={setTheme} />}></Route>
+                        <Route path="/email" element={<Email theme={theme} setTheme={setTheme} />}></Route>
+                        <Route path="/profile" element={<ProfilePage theme={theme} setTheme={setTheme} />}></Route>
+                        <Route path="/password" element={<Password theme={theme} setTheme={setTheme} />}></Route>
                         <Route path="/blog/:id" element={<BlogPage />}></Route>
-                        <Route path="/delete-account" element={<DeleteAccount theme = {theme} setTheme = {setTheme} />}></Route>
-                        <Route path= '/answer' element = {<AnsweringPage theme = {theme} setTheme = {setTheme} />}></Route>
-                        <Route path= '/upload' element = {<UploadPage/>}></Route>
-                        
+                        <Route path="/delete-account" element={<DeleteAccount theme={theme} setTheme={setTheme} />}></Route>
+                        <Route path='/answer' element={<AnsweringPage theme={theme} setTheme={setTheme} />}></Route>
+                        <Route path='/upload' element={<UploadPage />}></Route>
+
                     </Routes>
-                    </ThemeContext.Provider>
-                </Router>
-                {/* <MainBar /> */}
-            </div>
+                </ThemeContext.Provider>
+            </Router>
+            {/* <MainBar /> */}
+        </div>
     );
 }
 
--- src/Component/OTP Page/OTP.js
+++ src/Component/OTP Page/OTP.js
@@ -1,34 +1,95 @@
-import React from 'react'
-import "../../styles/Sheets/Style.css"
 import LoginDesign from '../../Assets/LoginDesign.svg';
 import Back from '../../Assets/Back.png';
-function OTP() {
-  return (
-    <div className='main1'>
-        
-    <div className='Welcome'>
-    <div className='BackButton'>
-            <button><img src= {Back} alt="" /></button>
-    </div>
-        <p className='txt1'>Verification Code</p>
-        <p className='req2'> <p className='random'>Please enter 4-digit code sent to</p>  <br /> <p className='mailId'> abc@email.com</p></p>
 
-    <div className='codeBox'>
-        <div className='BOX'><form action=""><input min= "0" max= "1" type="text" /></form></div>
-        <div className='BOX'><input min= "0" max= "1" type="text" /></div>
-        <div className='BOX'><input min= "0" max= "1" type="text" /></div>
-        <div className='BOX'><input min= "0" max= "1" type="text" /></div>
-    </div>   
-        <p className='Code'>Didn't receive the code?</p>
-        <div className='VerifyButton'>
-            <button>Verify your Email</button>
+
+import React, { useState, useEffect } from 'react';
+
+const OTP = ({ length = 4 }) => {
+
+    const [otp, setOTP] = useState(Array(length).fill(''));
+
+    const handleChange = (index, value) => {
+        if (!isNaN(value) && value !== '' && index < length) {
+            const newOTP = [...otp];
+            newOTP[index] = value;
+            setOTP(newOTP);
+        }
+    };
+
+    const handleKeyUp = (event, index) => {
+        if (event.key === 'Backspace' && otp[index] === '' && index > 0) {
+            const newOTP = [...otp];
+            newOTP[index - 1] = '';
+            setOTP(newOTP);
+        }
+    };
+
+
+    useEffect(() => {
+        const onComplete = (otp) => {
+            console.log('Your OTP is', otp);
+            setOTP(Array(length).fill(''));
+        };
+
+        const focusNextInput = (index) => {
+            if (index < length - 1) {
+                const nextInput = document.getElementById(`input-${index + 1}`);
+                if (nextInput) {
+                    nextInput.focus();
+                }
+            }
+        };
+
+        if (otp.some(val => val === '')) {
+            const nextEmptyIndex = otp.findIndex(val => val === '');
+            focusNextInput(nextEmptyIndex - 1);
+        } else {
+            onComplete(otp.join(''));
+        }
+    }, [otp, length]);
+
+
+    const inputs = Array.from({ length }, (_, index) => (
+        <input
+            key={index}
+            type="text"
+            maxLength={1}
+            value={otp[index]}
+            onChange={e => handleChange(index, e.target.value)}
+            onKeyUp={e => handleKeyUp(e, index)}
+            id={`input-${index}`}
+            className='BOX'
+            inputMode='numeric'
+            autoComplete='off'
+        />
+    ));
+
+    // return <div className="otp-input">{inputs}</div>;
+    return (
+        <div className='main1'>
+            <div className='Welcome'>
+                <div className='BackButton'>
+                    <button><img src={Back} alt="" /></button>
+                </div>
+                <p className='txt1'>Verification Code</p>
+                <p className='req2'>
+                    <p className='random'>Please enter 4-digit code sent to</p>
+                    <br />
+                    <p className='mailId'> abc@email.com</p>
+                </p>
+                <div className='codeBox'>
+                    {inputs}
+                </div>
+                <p className='Code'>Didn't receive the code?</p>
+                <div className='VerifyButton'>
+                    <button>Receive new Code</button>
+                </div>
+            </div>
+            <div className='Design'>
+                <img className='img' src={LoginDesign} alt="" />
+            </div >
         </div>
-    </div>
-    <div className='Design'>
-        <img className='img' src= {LoginDesign} alt="" />
-    </div >
-</div>
-  )
-}
+    )
+};
 
-export default OTP
+export default OTP;
