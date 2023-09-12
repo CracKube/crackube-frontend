59939765a410ab381ad89d476fe18e2cbd7adf97
Geen-19 <angelinedias53@gmail.com>
2023-09-12 11:00:52 +0530

    Deploy Site

--- src/Component/Card/Card1/CardAuthor1.js
+++ src/Component/Card/Card1/CardAuthor1.js
@@ -1,6 +1,8 @@
 import styles from './CardAuthor1.module.css';
 import { bookmarkUrl } from '../../Constants';
-import BlogShare from '../../../Assets/Blog'
+import BlogShare from '../../../Assets/BlogShare.svg'
+import BlogOptions from '../../../Assets/BlogOption.svg'
+import BlogLike from '../../../Assets/BlogLike.svg'
 export default function CardAuthor1(props){
     const ques = props.author.length;
     const val = props.author.slice(0,25);
@@ -22,9 +24,9 @@ export default function CardAuthor1(props){
                 <div style={{fontWeight: '50'}}>{props.modified}</div>
             </div>
             <div className={styles.bookmark}>
-                <a href=""><img src="" alt="" /></a>
-                <a href=""><img src="" alt="" /></a>
-                <a href=""><img src={bookmarkUrl} alt="" /></a>
+                <a href=""><img src= {BlogLike} alt="" /></a>
+                <a href=""><img src= {BlogShare} alt="" /></a>
+                <a href=""><img src={BlogOptions} alt="" /></a>
             </div>
         </div>
     );
