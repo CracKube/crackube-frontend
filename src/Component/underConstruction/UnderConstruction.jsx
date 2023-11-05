6113d2632998959a04d585a8975b27fff7647ce1
Divy Parekh <divyparekh1810@gmail.com>
2023-11-05 15:19:09 +0530

    Underconstruction page - without color change

---
+++ src/Component/underConstruction/UnderConstruction.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import "./UnderConstruction.css";
+const UnderConstruction = () => {
+  return (
+    <div>
+      <main>
+        <section class="advice">
+          <h1 class="advice__title">Page under construction </h1>
+          <p class="advice__description">
+            <span></span> <strong>BUILDING</strong> not finished yet
+          </p>
+        </section>
+        <section class="city-stuff">
+          <ul class="skyscrappers__list">
+            <li class="skyscrapper__item skyscrapper-1"></li>
+            <li class="skyscrapper__item skyscrapper-2"></li>
+            <li class="skyscrapper__item skyscrapper-3"></li>
+            <li class="skyscrapper__item skyscrapper-4"></li>
+            <li class="skyscrapper__item skyscrapper-5"></li>
+          </ul>
+          <ul class="tree__container">
+            <li class="tree__list">
+              <ul class="tree__item tree-1">
+                <li class="tree__trunk"></li>
+                <li class="tree__leaves"></li>
+              </ul>
+              <ul class="tree__item tree-2">
+                <li class="tree__trunk"></li>
+                <li class="tree__leaves"></li>
+              </ul>
+              <ul class="tree__item tree-3">
+                <li class="tree__trunk"></li>
+                <li class="tree__leaves"></li>
+              </ul>
+              <ul class="tree__item tree-4">
+                <li class="tree__trunk"></li>
+                <li class="tree__leaves"></li>
+              </ul>
+              <ul class="tree__item tree-5">
+                <li class="tree__trunk"></li>
+                <li class="tree__leaves"></li>
+              </ul>
+              <ul class="tree__item tree-6">
+                <li class="tree__trunk"></li>
+                <li class="tree__leaves"></li>
+              </ul>
+              <ul class="tree__item tree-7">
+                <li class="tree__trunk"></li>
+                <li class="tree__leaves"></li>
+              </ul>
+              <ul class="tree__item tree-8">
+                <li class="tree__trunk"></li>
+                <li class="tree__leaves"></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="crane__list crane-1">
+            <li class="crane__item crane-cable crane-cable-1"></li>
+            <li class="crane__item crane-cable crane-cable-2"></li>
+            <li class="crane__item crane-cable crane-cable-3"></li>
+            <li class="crane__item crane-stand"></li>
+            <li class="crane__item crane-weight"></li>
+            <li class="crane__item crane-cabin"></li>
+            <li class="crane__item crane-arm"></li>
+          </ul>
+          <ul class="crane__list crane-2">
+            <li class="crane__item crane-cable crane-cable-1"></li>
+            <li class="crane__item crane-cable crane-cable-2"></li>
+            <li class="crane__item crane-cable crane-cable-3"></li>
+            <li class="crane__item crane-stand"></li>
+            <li class="crane__item crane-weight"></li>
+            <li class="crane__item crane-cabin"></li>
+            <li class="crane__item crane-arm"></li>
+          </ul>
+          <ul class="crane__list crane-3">
+            <li class="crane__item crane-cable crane-cable-1"></li>
+            <li class="crane__item crane-cable crane-cable-2"></li>
+            <li class="crane__item crane-cable crane-cable-3"></li>
+            <li class="crane__item crane-stand"></li>
+            <li class="crane__item crane-weight"></li>
+            <li class="crane__item crane-cabin"></li>
+            <li class="crane__item crane-arm"></li>
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default UnderConstruction;
